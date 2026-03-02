b49f6b1dcd2f29daba0963495e447499fe5308d3
Drew Lawrence <drew.lawrence442@gmail.com>
2026-03-02 15:54:52 +1100

    build: enable packaging for lvm-san charms

--- charms/storage/cinder-volume-lvm-san/lib/charms/lvm_san/v0/lvm_san_backend.py
+++ charms/storage/cinder-volume-lvm-san/lib/charms/lvm_san/v0/lvm_san_backend.py
@@ -12,7 +12,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 0
+LIBPATCH = 1
 
 import ipaddress
 import json
